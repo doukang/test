8fe4e1d3ae61a6f2b809eed9ad8c184f7d76838d
hyk <2062336753@qq.com>
2024-10-09 18:38:07 +0800

    branchfirst

--- src/helloqq.java
+++ src/helloqq.java
@@ -1,5 +1,5 @@
 public class helloqq {
     public static void main(String[] args) {
-        System.out.println("Hello from the feature branch!");
+        System.out.println("Hello from the feature branchfirst!");
     }
 }
